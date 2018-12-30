75460cccf9ff009f812b05552d09792a34a2624e
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2018-12-30 13:55:06 -0800

    Update App.java

--- App.java
+++ App.java
@@ -16,121 +16,149 @@ public class App extends JFrame {
         frame.setTitle("Java Application");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(game);
+
     }
 }
 
 
 class Game extends JPanel implements KeyListener, ActionListener {
     private static Timer timer;
-    private static int ballX= 10, ballY = 10, ballXdir = 3, ballYdir = 2;
-    private static int width_divider = 10;
+    private static int ballX, ballY, ballXdir, ballYdir, ball_width, ball_height;
     private static int padX, padY, width, height;
     private int moveSpeed = 30;
-    private int delay = 10;
+    private boolean isStarted = false, isBallAlive = true, isTouched = false;
+
+
 
 
 
 
 
     Game(){
-        width = 60;
-        height = 5;
-        padX = (600/2)-(width);
-        padY = 600-50;
+        int delay = 10;
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
         timer = new Timer(delay, this);
+
+        // Player
+        width = 60;
+        height = 5;
+        padX = (600/2)-width;
+        padY = 500;
+
+        // Ball
+        ball_width = 20;
+        ball_height = 20;
+        ballX = (600/2)-(ball_width+20);
+        ballY = 300;
+
+
+        // Ball speed direction
+        ballXdir = 3;
+        ballYdir = 3;
+
         timer.start();
     }
 
 
+
+
     public void paint(Graphics g){
-
-
-
+        isStarted = true;
         g.setColor(Color.black);
         g.fillRect(0,0, super.getWidth(), super.getHeight());
 
         // ball
         g.setColor(Color.white);
-        g.fillOval(ballX, ballY, 20, 20);
+        g.fillOval(ballX, ballY, ball_width, ball_height);
 
         // Padel
         g.setColor(Color.cyan);
-        g.drawRect(padX, padY, width, height);
+        g.draw3DRect(padX, padY, width, height, true);
 
+
+
+        if(!isBallAlive){
+            g.setColor(Color.red);
+            g.drawString("YOU LOST", 600/2, 400);
+        }
         g.dispose();
 
 
     }
     public void actionPerformed(ActionEvent e) {
-       timer.start();
-       ballX += ballXdir;
-       ballY += ballYdir;
-//       padX += moveSpeed;
+        timer.start();
+
+        if(isBallAlive) {
+            ballY += ballYdir;
 
 
-       if(ballX >= super.getWidth()-20){
-           ballXdir = -ballXdir;
-       }
+            if(ballX >= super.getWidth()-20){
+                ballXdir = -ballXdir;
+            }
 
-       if(ballX <= 0 ){
-           ballXdir = -ballXdir;
-       }
+            if(ballX <= 0 ){
+                ballXdir = -ballXdir;
+            }
 
+            if(ballY <= 0 ){
+                ballYdir = -ballYdir;
+            }
 
-            // floor fallback
-//        if(ballY >= super.getHeight()-25){
-//            ballYdir = -ballYdir;
-//        }
+        }
 
-        if(ballY <= 0 ){
-            ballYdir = -ballYdir;
+        // floor fallback
+        if(ballY >= 600){
+           isBallAlive = false;
         }
 
 
 
-        if(padX <=0 ) {
-           padX = 1;
-        }
-        if(padX >= super.getWidth()-width){
-            padX = super.getWidth()-width;
 
+
+        if(isStarted && isBallAlive) {
+            if (padX <= 0) {
+                padX = 1;
+            }
+
+            if (padX >= super.getWidth() - width) {
+                padX = super.getWidth() - (width + 5);
+
+            }
         }
 
 
         // Collision ball and pad
 
-        if((ballX) >= (padX-16) && (ballX) <= (padX+width) && (ballY) >= (padY-20) && (ballY-20) <= (padY+height)){
+        if((ballX) >= (padX-(ball_height-4)) && (ballX) <= (padX+width) && (ballY) >= (padY-ball_height) && (ballY-ball_height) <= (padY+height)){
             ballYdir = -ballYdir;
-            System.out.println("TOUCHED");
+            isTouched = true;
         }
 
+        if(isTouched && isBallAlive){
+                 ballX += ballXdir;
+        }
+
+        // Re drawing after every delay timer
         repaint();
     }
 
 
-
-
-
-    public void keyReleased(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        // Right Arrow
+        if(( key == KeyEvent.VK_RIGHT  || key == KeyEvent.VK_D) && isBallAlive) {
             padX += moveSpeed;
         }
-        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+
+        // Left arrow
+        if( ( key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A ) && isBallAlive) {
             padX -= moveSpeed;
         }
     }
+
+
+    public void keyReleased(KeyEvent e) {}
     public void keyTyped(KeyEvent e) {}
-    public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-
-                padX += moveSpeed;
-
-        }
-        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-            padX -= moveSpeed;
-        }
-    }
 }
